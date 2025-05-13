48ca2a3392749ea36b3ee767ce1660e61843750f
Vinicius Alvim <vini0191@gmail.com>
2025-05-13 11:13:16 -0300

    Add image to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
+import Image from 'next/image';
+
 function Home() {
-  return <h1>Irma, I love you! ❤️🥰</h1>;
+  return (
+    <>
+      <h1>Irma, I love you! ❤️🥰</h1>
+      <Image
+        src="/images/irma_and_vinicius.jpeg"
+        width={4032}
+        height={3024}
+        alt="Irma and Vinicius"
+        style={{
+          width: 'auto',
+          height: 'auto',
+          maxWidth: '75vw',
+          maxHeight: '75vh',
+        }}
+      />
+    </>
+  );
 }
 
 export default Home;
